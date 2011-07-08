92037fb7d49291c00ffbe0fb1c36afbd0e89649c
Christer Edvartsen <cogo@starzinger.net>
2011-07-08 12:00:24 +0200

    Added main application class

---
+++ library/PHSA/Application.php
@@ -0,0 +1,31 @@
+<?php
+namespace PHSA;
+
+use PHSA\Command\ListReposesCommand;
+use Symfony\Component\Console;
+
+/**
+ * Main application
+ */
+class Application extends Console\Application {
+    /**
+     * Class constructor
+     */
+    public function __construct() {
+        parent::__construct('PHSA', Version::getVersionNumber());
+
+        // Register commands
+        $this->add(new ListReposesCommand());
+
+        // Add global options
+        $this->getDefinition()->addOption(
+            new Console\Input\InputOption(
+                'config-file',
+                null,
+                Console\Input\InputOption::VALUE_OPTIONAL,
+                'Path to configuration file',
+                PHSA_CONFIG_FILE
+            )
+        );
+    }
+}
