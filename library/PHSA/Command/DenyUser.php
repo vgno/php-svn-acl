99248db501c91407eb9a0274a536f8e5d8f34760
Christer Edvartsen <cogo@starzinger.net>
2011-07-10 13:56:39 +0200

    Added a command that can be used to deny users access

---
+++ library/PHSA/Command/DenyUser.php
@@ -0,0 +1,69 @@
+<?php
+namespace PHSA\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Command used to list the ACLs stored in the database
+ */
+class DenyUser extends BaseCommand {
+    /**
+     * Class constructor
+     */
+    public function __construct() {
+        parent::__construct('deny-user');
+        $this->setDescription('Deny a user');
+        $this->setHelp('Deny a user access to one or more repositories');
+
+        $this->addArgument('user', InputArgument::REQUIRED, 'Username to deny');
+        $this->addArgument('path', InputArgument::IS_ARRAY, 'Optional paths to deny the user access to in the specified repositories');
+
+        $this->addOption('repos', null, InputOption::VALUE_OPTIONAL, 'Comma separated list of repositories to deny the user access to');
+        $this->addOption('all', null, InputOption::VALUE_NONE, 'Use this option to deny access to all currently available repositories');
+    }
+
+    /**
+     * Execute the commend
+     *
+     * @see \Symfony\Components\Console\Command\Command::execute()
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $dbDriver = $this->configuration['database']['driver'];
+
+        $username = $input->getArgument('user');
+        $paths = $input->getArgument('path');
+
+        $repositories = array();
+
+        if ($input->getOption('all')) {
+            // Fetch list of repositories from the subversion driver
+            $svnDriver = $this->configuration['subversion']['driver'];
+            $repositories = $svnDriver->listRepositories($this->configuration['svnParentDir']);
+        } else if ($input->getOption('repos')) {
+            $tmp = $input->getOption('repos');
+            $repositories = array_map('trim', explode(',', $tmp));
+        } else {
+            throw new \InvalidArgumentException('Specify either --all or --repos');
+        }
+
+        $rules = 0;
+
+        foreach ($repositories as $repos) {
+            if (empty($paths)) {
+                $rules++;
+                $dbDriver->denyUser($username, $repos);
+            } else {
+                // Paths have been specified. Add all paths to all repositories
+                foreach ($paths as $path) {
+                    $rules++;
+                    $dbDriver->denyUser($username, $repos, $path);
+                }
+            }
+        }
+
+        $output->writeln($rules . ' rule' . ($rules !== 1 ? 's' : '') . ' have been added to the database');
+    }
+}
