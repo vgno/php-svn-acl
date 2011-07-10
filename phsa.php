77c9510c616f7d82a7a601e2d2d3e1a03357ecda
Christer Edvartsen <cogo@starzinger.net>
2011-07-10 10:31:52 +0200

    Added Console and ClassLoader components from Symfony2 (2.0.0RC1)

--- phsa.php
+++ phsa.php
@@ -1,20 +1,16 @@
 #!/usr/bin/env php
 <?php
 /** @see Symfony\Component\ClassLoader\UniversalClassLoader */
-require_once 'Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__ . '/library/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-
-if (strpos('@php_bin@', '@php_bin') === 0) {
-    $loader->registerNamespace('PHSA', __DIR__ . '/library');
-    define('PHSA_CONFIG_FILE', __DIR__ . '/config/config.php');
-} else {
-    $loader->registerNamespace('PHSA', explode(PATH_SEPARATOR, get_include_path()));
-    define('PHSA_CONFIG_FILE', '/etc/phsa/config.php');
-}
-
-$loader->registerNamespace('Symfony', explode(PATH_SEPARATOR, get_include_path()));
+$loader->registerNamespaces(array(
+    'Symfony' => __DIR__ . '/library',
+    'PHSA'    => __DIR__ . '/library',
+));
 $loader->register();
+
+define('PHSA_CONFIG_FILE', __DIR__ . '/config/config.php');
 
 $application = new PHSA\Application();
 $application->run();
